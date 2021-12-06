9f5d061acb5a355209b1a7cdd518c6b34593bbf6
nikogianna <nikogianna19@gmail.com>
2021-12-06 05:21:03 -0600

    change port to 8080

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+//const port = 3000
+const port = 8080
 
 app.get('/', (_, res) => res.sendFile(path.join(__dirname, 'index.html')))
 //app.use(express.static(__dirname + '/public'));
