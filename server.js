c0be36acfeab91572a31ff4612bf48c452774606
nikogianna <nikogianna19@gmail.com>
2021-12-06 02:15:02 -0600

    first cat commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.get('/', (_, res) => res.sendFile(path.join(__dirname, 'index.html')))
+//app.use(express.static(__dirname + '/public'));
+
+
+app.listen(port, () => console.log(`Listening on port ${port}...`))
+//app.listen(process.env.PORT || 8080);
